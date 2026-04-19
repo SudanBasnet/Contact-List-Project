7f2bee34a9d929154e032e311a17c062270dfc3c
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-19 18:04:06 +1000

    fetching and pushing to inner HTML

--- script.js
+++ script.js
@@ -38,7 +38,86 @@ const fetchUsers = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   userList = data.results;
-  console.log(userList);
+
+  //hide the spinner
+  document.querySelector(".showSpinner").style.display = "none";
+
+  //show the user
+  displayContactList(userList);
 };
 
 fetchUsers(apiEP);
+
+//display contact list
+
+const displayContactList = (userList) => {
+  console.log(userList);
+  document.getElementById("list").style.display = "block";
+  let str = "";
+  userList.map((item, i) => {
+    str += `                  <div class="accordion-item">
+                    <h2 class="accordion-header">
+                      <button
+                        class="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapse${i}"
+                        aria-expanded="false"
+                        aria-controls="collapse${i}"
+                      >
+                        <img
+                          src="${item.picture.large}"
+                          alt=""
+                          width="50px"
+                          class="rounded-circle"
+                        />
+                        <div class="ms-2">
+                          <div class="fw-bolder">${item.name.title} ${item.name.first} ${item.name.last}</div>
+                          <small>${item.location.street.name} ${item.location.street.number}  </small>
+                        </div>
+                      </button>
+                    </h2>
+                    <div
+                      id="collapse${i}"
+                      class="accordion-collapse collapse"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div
+                        class="accordion-body d-flex flex-column align-items-center"
+                      >
+                        <img
+                          src="${item.picture.large}"
+                          alt=""
+                          width="150px"
+                          class="rounded-circle"
+                        />
+                        <div>
+                          <div class="fw-bolder">
+                            <i class="bi bi-person-circle"></i> ${item.name.title} ${item.name.first} ${item.name.last}
+                          </div>
+                          <div>
+                            <a href="tel:${item.cell}"
+                              ><i class="bi bi-phone-fill"></i>${item.cell}</a
+                            >
+                          </div>
+                          <div>
+                            <a href="mailto:${item.email}">
+                              <i class="bi bi-envelope"></i> ${item.email}</a
+                            >
+                          </div>
+                          <div>
+                            <a
+                              href="https://www.google.com/maps/place/${item.location.street.name}+${item.location.street.number}+
+                              ${item.location.state}+ ${item.location.country}"
+                              target="_blank"
+                              ><i class="bi bi-globe-asia-australia-fill"></i> ${item.location.street.name} ${item.location.street.number}
+                             ${item.location.state} ${item.location.country} </a
+                            >
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`;
+  });
+  document.getElementById("useraccordion").innerHTML = str;
+};
