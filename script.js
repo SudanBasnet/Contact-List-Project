14123e9b6f9cf9148a2f16d64c6148e3eb7ff78b
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-19 17:27:40 +1000

    fetching data

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const apiEP = "https://randomuser.me/api?results=5";
 //slide to go to app screen
+let userList = [];
 
 const slider = document.getElementById("mySlider");
 slider.addEventListener("change", (e) => {
@@ -18,3 +20,25 @@ const dispAppScreen = () => {
   //show app screen
   document.querySelector(".appScreen").style.display = "block";
 };
+
+const fetchUsers = async (url) => {
+  //fetch users
+  //promise method
+  // fetch(url)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  //async await
+  const response = await fetch(url);
+  const data = await response.json();
+  userList = data.results;
+  console.log(userList);
+};
+
+fetchUsers(apiEP);
